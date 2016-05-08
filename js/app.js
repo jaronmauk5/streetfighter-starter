2caaf92d10c40326af3676ed495e2164aeb3438e
Jaron Mauk <jaronmauk5@gmail.com>
2016-05-08 14:32:06 -0700

    added sound effect

--- js/app.js
+++ js/app.js
@@ -9,13 +9,27 @@ $(document).ready(function() {
 	})
 	.mousedown(function() {
 		//play hadouken sound effect
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//animate hadouken to the right of the screen
+		$('.hadouken').finish().show().animate(
+			{'left':'1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left','520px');
+			});
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+
+	
 });
+
+function playHadouken() {
+			$('#hadouken-sound')[0].volume = 0.5;
+			$('#hadouken-sound')[0].load();
+			$('#hadouken-sound')[0].play();
+		}
